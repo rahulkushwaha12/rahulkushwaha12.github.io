3d9ee37d9c7414f7eef33517a3355110d3fa0f8a
Rahul Kushwaha <kushwaharesonance@gmail.com>
2017-09-24 15:18:45 +0530

    created search_bar js component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCwimxfHXdIf-VlF_9zbtrYC3TTE7nbMmQ';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () =>{
+	return (
+		<div>
+			<SearchBar />
+		</div>
+		);
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
