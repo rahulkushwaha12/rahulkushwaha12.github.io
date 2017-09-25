0814b5c5221ae054997c2fcd47b4708257b23199
Rahul Kushwaha <kushwaharesonance@gmail.com>
2017-09-25 06:58:09 +0530

    added all components with embed youtube video

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
 			<div>
 			<input value={this.state.term}
 				onChange = {event => this.setState({term: event.target.value})}/>
-			Value of input: {this.state.term}
+			
 			</div>
 			);
 	}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCwimxfHXdIf-VlF_9zbtrYC3TTE7nbMmQ';
 
-const App = () =>{
-	return (
-		<div>
-			<SearchBar />
-		</div>
-		);
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			videos:[],
+			selectedVideo: null
+		};
+		YTSearch({key: API_KEY, term: 'newton'}, (videos)=>{
+			this.setState({
+				videos:videos,
+			selectedVideo:videos[0]
+		});
+		});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList videos = {this.state.videos}/>
+			</div>
+			);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
